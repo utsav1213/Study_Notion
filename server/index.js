dcbb855104fcc724b67cf6d6a82553d698bea263
utsav1213 <hiraprautsav54@gmail.com>
2025-02-28 12:24:45 +0530

    slkdfj

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-ten-zeta.vercel.app",
 		credentials:true,
 	})
 )
