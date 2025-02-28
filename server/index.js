cd97453cc4aad2ab8a61108d871144b98a4ffd57
utsav1213 <hiraprautsav54@gmail.com>
2025-02-28 18:14:58 +0530

    update in server

--- server/index.js
+++ server/index.js
@@ -21,13 +21,13 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
 	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
+		origin: "*",
+		credentials: true,
 	})
-)
-
+);
 app.use(
 	fileUpload({
 		useTempFiles:true,
